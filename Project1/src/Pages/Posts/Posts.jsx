9996659d1cf6ff1a85864d5536570bccd3c153fc
Leen-massarwy <84320246+Leen-massarwy@users.noreply.github.com>
2025-11-28 16:43:16 +0200

    move posts files to posts folder

--- Project1/src/Pages/Posts/Posts.jsx
+++ Project1/src/Pages/Posts/Posts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getPostsPerUser } from "../utils";
+import { getPostsPerUser } from "../../utils";
 import Post from "./Post";
 import AddPost from "../AddPost";
 
