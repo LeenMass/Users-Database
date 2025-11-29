170ab9a4b8bd3d31e0d1821e5de7b61dab0a2200
Leen Massarwy <115391076+LeenMass@users.noreply.github.com>
2025-11-29 12:51:11 +0200

    adding validation to todos functions

--- usersDb/src/Pages/Todos/todosUtils.js
+++ usersDb/src/Pages/Todos/todosUtils.js
@@ -2,7 +2,35 @@ import axios from "axios";
 
 const TODOS_URL = "https://jsonplaceholder.typicode.com/todos";
 
-const getTodosPerUser = (id) => axios.get(`${TODOS_URL}?userId=${id}`);
-const newTodoUser = (obj) => axios.post(`${TODOS_URL}`, obj)
+const getTodosPerUser = async (id) => {
+    try {
+        const response = await axios.get(`${TODOS_URL}?userId=${id}`)
 
+        if (response.status !== 201) {
+            throw new Error(`Failed to todos: ${response.status}`);
+        }
+
+        return response.data;
+
+    } catch (error) {
+        throw new Error("An error occurred while fething todos");
+    }
+
+}
+
+const newTodoUser = async (obj) => {
+    try {
+        const response = await axios.post(`${TODOS_URL}`, obj)
+
+
+        if (response.status !== 201) {
+            throw new Error(`Failed to create new todo: ${response.status}`);
+        }
+
+        return response.data;
+
+    } catch (error) {
+        throw new Error("An error occurred while creating new todo");
+    }
+}
 export { getTodosPerUser, newTodoUser };
