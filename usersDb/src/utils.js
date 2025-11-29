e0a5167c8b9b257dc1ac317be5e8af4de8db698a
Leen Massarwy <115391076+LeenMass@users.noreply.github.com>
2025-11-29 11:07:51 +0200

    update utils

--- usersDb/src/utils.js
+++ usersDb/src/utils.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const USERS_URL = "https://jsonplaceholder.typicode.com/users";
-const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 const TODOS_URL = "https://jsonplaceholder.typicode.com/todos";
 
 const getUsers = () => axios.get(USERS_URL);
