cf10035ee5591d60cddd05a857019261e5150996
Leen Massarwy <115391076+LeenMass@users.noreply.github.com>
2025-12-03 20:54:41 +0200

    using destructuring in AddPost component

--- usersDb/src/Pages/Posts/AddPost.jsx
+++ usersDb/src/Pages/Posts/AddPost.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { newPostUser } from "./postsUtils";
 
-const AddPost = (props) => {
+const AddPost = ({ openAddingPostWindow, userId, addPostToPostsArray }) => {
   const [newPost, setNewPost] = useState({
-    userId: props.userId,
+    userId: userId,
     title: "",
     body: "",
   });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setNewPost({ ...newPost, [name]: value });
@@ -14,7 +15,7 @@ const AddPost = (props) => {
   const addNewPost = async () => {
     try {
       const { data } = await newPostUser(newPost);
-      props.callback(data);
+      addPostToPostsArray(data);
       alert("Adding Post done Successfully");
     } catch (error) {
       alert("Failed to add new post");
@@ -22,7 +23,7 @@ const AddPost = (props) => {
   };
   return (
     <>
-      New Post-User{props.userId}
+      New Post-User{userId}
       <br />
       <div
         style={{
@@ -42,7 +43,7 @@ const AddPost = (props) => {
         <input type="text" name="body" onChange={handleChange} />
         <div>
           <button onClick={addNewPost}>Add</button>
-          <button onClick={props.func}>Cancel</button>
+          <button onClick={openAddingPostWindow}>Cancel</button>
         </div>
       </div>
     </>
